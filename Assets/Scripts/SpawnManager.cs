ae30b9d974bf1803b9ae05a5100b2296e4394839
TheRealRocSpartan73 <146456843+TheRealRocSpartan73@users.noreply.github.com>
2023-10-23 21:13:58 +0100

    4.2 Complete

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,20 +9,26 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        
+        //Create enemy sphere.
+        Instantiate(enemyPrefab, this.GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
         //Grab a random X and Y co-ordinate
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
 
         //Create a new Vector based on the random's
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
-
-        //Create enemy sphere.
-        Instantiate(enemyPrefab, randomPos, enemyPrefab.transform.rotation);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        return randomPos;
     }
 }
