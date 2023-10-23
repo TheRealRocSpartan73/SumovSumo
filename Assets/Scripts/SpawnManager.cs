dcd1f544c07adb4619afce90a0654f84e73de339
TheRealRocSpartan73 <146456843+TheRealRocSpartan73@users.noreply.github.com>
2023-10-23 21:05:58 +0100

    4.2.5 Complete

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    private float spawnRange = 9.0f; //Range from origin for randoms
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, new Vector3(0,0,6), enemyPrefab.transform.rotation);
+        //Grab a random X and Y co-ordinate
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+        //Create a new Vector based on the random's
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        //Create enemy sphere.
+        Instantiate(enemyPrefab, randomPos, enemyPrefab.transform.rotation);
     }
 
     // Update is called once per frame
